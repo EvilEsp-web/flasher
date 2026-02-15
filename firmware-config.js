f90c27034b80435580c7020f44c69ee6823a713c
EvilEsp-web <z16846751@gmail.com>
2026-02-15 15:02:34 +0100

    Add files via upload

---
+++ firmware-config.js
@@ -0,0 +1,8 @@
+// Firmware Config - EDYTUJ TO!
+const hardcodedFirmware = [
+  {id: "nrf", name: "nRF_Jammer_V3", version: "3.0", category: "Bad Firmware", platform: "esp32", manifest: "nRF_Jammer_V3_bin/manifest.json"},
+  {id: "polishai", name: "uart AI", version: "1.0", category: "AI", platform: "esp32", manifest: "Polish_AI_SD/manifest.json"},
+  {id: "eviltools", name: "Evil ESP32 Tools", version: "1.0", category: "Bad Firmware", platform: "esp32", manifest: "evil_esp32_tools_bin/manifest.json"} ,
+   {id: "eviltools", name: "Evil ESP32 Tools", version: "1.0", category: "Kutas", platform: "esp32", manifest: "evil_esp32_tools_bin/manifest.json"} 
+
+];
